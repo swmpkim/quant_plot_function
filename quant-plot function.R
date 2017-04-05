02457e1608cd935bfb3b99060e5ddc35ebb4db1f
swmpkim <kimberly.cressman@dmr.ms.gov>
2017-04-05 10:19:42 -0500

    attempts to make function more flexible for users
    not really generalizing yet; trying to clean up the quantile generating
    portion of the code so there's less repetition. lots of "thinking out
    loud" on how to generalize the function.

--- quant-plot function.R
+++ quant-plot function.R
@@ -1,4 +1,17 @@
-# required packages: dplyr, lubridate, ggplot2
+# for testing: ----
+# library(SWMPr)
+# library(dplyr)
+# library(lubridate)
+# library(ggplot2)
+# library(tibble)
+# setwd("data visualization")
+# path <- "C:/Users/kimberly.cressman/Desktop/Main Docs/Data-latest"
+# bhwq <- import_local(path, 'gndbhwq', trace=TRUE)
+# bhwq <- qaqc(bhwq, qaqc_keep = c(0,4))
+
+
+# function ----
+# required packages: dplyr, lubridate, ggplot2, tibble
 # assuming data originally imported and processed with SWMPr
 
 # definitions:
@@ -43,21 +56,57 @@ quant.plot <- function(data, paramtoplot, yr, yrstart, yrend) {
   # work with quantiles ----
   
   # do some subsetting and sorting on backdrop data (using dplyr)
+  # need to pull this out so a column can be named 'paramtoplot'
+  # which makes later coding easier
   dat_quantiles <- select_(dat_backdrop, paramtoplot = paramtoplot, 'monthday', 'year')
-  by_doy <- group_by(dat_quantiles, monthday)
+  
+  # by_doy <- group_by(dat_quantiles, monthday)
   
   # subset featured data (using dplyr)
   dat_feature <- select_(dat_feature, paramtoplot = paramtoplot, 'monthday', 'year')
   
-  # generate a summary table (using dplyr)
-  doy_sum <- summarise(by_doy,
-                       min = quantile(paramtoplot, 0, na.rm=TRUE),
-                       pct10 = quantile(paramtoplot, 0.10, na.rm=TRUE),
-                       pct25 = quantile(paramtoplot, 0.25, na.rm=TRUE),
-                       pct75 = quantile(paramtoplot, 0.75, na.rm=TRUE),
-                       pct90 = quantile(paramtoplot, 0.90, na.rm=TRUE),
-                       max = quantile(paramtoplot, 1, na.rm=TRUE)
-  )
+  # generate a summary table (using dplyr and tibble) ----
+  
+  # first gather quantiles for every monthday
+  # could probably make this quantile(x, n, na.rm=TRUE)
+  # where n is a vector that the user inputs at the beginning of the function to pull out whatever percentiles they want
+  doy_sum2 <- tapply(dat_quantiles$paramtoplot, dat_quantiles$monthday, function(x) quantile(x, c(0, 0.25, 0.75, 1), na.rm=TRUE))
+  # that spits out a list.
+  # pull the list together with do.call and rbind:
+  doy_sum3 <- do.call(rbind, doy_sum2)
+  # but that's a matrix, so make it a data frame:
+  doy_sum3 <- data.frame(doy_sum3)
+  # and turn the row names into the column "monthday", using tibble::rownames_to_column(), and name it doy_sum
+  doy_sum <- rownames_to_column(doy_sum3, var="monthday")
+  
+  # the above code probably lends itself to more flexibility and less error than the below stuff, but I'm not crazy about how the names come out
+  # could hard code better ones but that defeats the purpose of flexibility
+  # maybe user needs to input both a vector of desired quantiles AND a vector of desired names for those quantiles?
+  
+  # function(data, paramtoplot, yr, yrstart, yrend)
+  # becomes
+  # function(data, paramtoplot, yr, yrstart, yrend, toquant = c(0, 0.25, 0.75, 1), quantnames = c('min', '25%', '75%', 'max'), ...)
+
+  # but then how does that translate down into the ribbon plot?
+  # need to specify other things too; this could become a long function call when defaults are specified for everything:
+  # outer ribbon (currenty hard coded as min and max)
+  # inner ribbon (currently hard coded as 25th - 75th percentiles)
+  # outer ribbon color (currently hard coded as light gray)
+  # innter ribbon color (currently hard coded as gray65)
+  # feature year line color (currently hard coded as red3)
+  # axis titles
+  # main title
+  
+    
+  
+  # doy_sum <- summarise(by_doy,
+  #                      min = quantile(paramtoplot, 0, na.rm=TRUE),
+  #                      pct10 = quantile(paramtoplot, 0.10, na.rm=TRUE),
+  #                      pct25 = quantile(paramtoplot, 0.25, na.rm=TRUE),
+  #                      pct75 = quantile(paramtoplot, 0.75, na.rm=TRUE),
+  #                      pct90 = quantile(paramtoplot, 0.90, na.rm=TRUE),
+  #                      max = quantile(paramtoplot, 1, na.rm=TRUE)
+  # )
   
   # join the two data frames into one (using dplyr)
   all_doy <- full_join(dat_feature, doy_sum, by = "monthday")
@@ -67,6 +116,9 @@ quant.plot <- function(data, paramtoplot, yr, yrstart, yrend) {
   all_doy$monthday <- paste0(all_doy$monthday, "-2008")
   all_doy$monthday <- mdy(all_doy$monthday)
   
+  
+  # as of 4-5-17 10:15, everything to this point works but the plot won't
+  # because names are different from the original script
   
   # make a ribbon plot ----
   
